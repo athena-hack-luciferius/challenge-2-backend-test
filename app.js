773916c032e02c1df9554acba4ed7006e7e44188
Tobias Wilker <twilker@phoenixcontact.com>
2022-06-15 07:04:28 +0200

    Complete haiku generation

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 //load the express module
 const express = require('express'); // this function returns an object of type express
 const { Configuration, OpenAIApi } = require("openai");
+const cors = require('cors');
+const borsh = require('borsh');
+const nearAPI = require("near-api-js");
+const nacl = require("tweetnacl");
+const sha256 = require('js-sha256');
+const { keyStores } = nearAPI;
+const keyStore = new keyStores.InMemoryKeyStore();
+
+const { connect } = nearAPI;
+
+const config = {
+  networkId: "testnet",
+  keyStore, 
+  nodeUrl: "https://rpc.testnet.near.org",
+  walletUrl: "https://wallet.testnet.near.org",
+  helperUrl: "https://helper.testnet.near.org",
+  explorerUrl: "https://explorer.testnet.near.org",
+  contract: ""
+};
+let near = null;
+
+connect(config).then(n => {near=n});
  
 //by convention we call the object app
 const app = express();
 
 //adding a piece of middleware to allow us to parse json objects
 app.use(express.json());
+app.use(cors())
 
 const configuration = new Configuration({
     apiKey: process.env.OPEN_AI_API_KEY,
   });
 const openai = new OpenAIApi(configuration);
+
+let haikus = new Map();
 	
 //Defining a Route
  
@@ -23,14 +48,25 @@ app.get('/', function(_, res){  //when we get an http get request to the root/ho
     res.send("API Version 1.0");
 });
 
-const getHaiku = async (req) => {
+const getHaiku = async (message) => {
+    var BinArrayToJson = function(binArray)
+    {
+        var str = "";
+        for (var i = 0; i < binArray.length; i++) {
+            str += String.fromCharCode(parseInt(binArray[i]));
+        }
+        return JSON.parse(str)
+    }
+
+    message = BinArrayToJson(message);
+
     const randomSeed = Math.floor(Math.random() * Math.pow(10, Math.floor(Math.random() * 12)));
     let prompt = `${randomSeed}\nWrite a haiku`;
-    if(req.body.adjective){
-        prompt = `${randomSeed}\nWrite a ${req.body.adjective} haiku`;
+    if(message.adjective){
+        prompt = `${randomSeed}\nWrite a ${message.adjective} haiku`;
     }
-    if(req.body.topic){
-        prompt += ` about ${req.body.topic}`;
+    if(message.topic){
+        prompt += ` about ${message.topic}`;
     }
     console.log(prompt);
     const response = await openai.createCompletion("text-davinci-002", {
@@ -48,27 +84,90 @@ const getHaiku = async (req) => {
     haiku = haiku.split('\n').slice(-3).join(" / ");
     return haiku;
 }
+
+const verify = async (message, signature, accountId) => {
+    const validSignature = nacl.sign.detached.verify(Uint8Array.from(sha256.sha256.array(message)), Uint8Array.from(signature.signature), Uint8Array.from(signature.publicKey.data));
+
+    signature.publicKey.data = borsh.baseEncode(signature.publicKey.data);
+    let owns = true;
+    try {
+        await near.connection.provider.query({
+            request_type: "view_access_key",
+            finality: "final",
+            account_id: accountId,
+            public_key: signature.publicKey.data,
+          });
+    } catch (error) {
+        owns=false;
+    }
+    
+    return owns && validSignature;
+}
+
+const verifyOwnership = async (accountId, id) => {
+    const response = await near.connection.provider.query({
+        request_type: "call_function",
+        finality: "final",
+        account_id: "haiku_nft.cryptosketches.testnet",
+        method_name: "nft_token",
+        args_base64: Buffer.from(JSON.stringify({token_id: id})).toString('base64'),
+      });
+    if(response.error){
+        return false;
+    }
+    const token = JSON.parse(String.fromCharCode(...response.result))
+    return token.owner_id === accountId;
+}
  
 //when we route to /courses
-app.post('/get-haiku', async (req, res) => {
-    /*if(!req.body.name ||
-        !req.body.year.toString().match(/^[0-9]{4}$/g) ||
-        !req.body.rating.toString().match(/^[0-9]\.[0-9]$/g))
-    {
+app.post('/get-haiku', async (req, res, next) => {
+    if(!req.body.message ||
+       !req.body.signature ||
+       !req.body.accountId ||
+       !req.body.id){
         res.status(400);
-        res.json({message: "Bad Request"});
-    }*/
-
-    haikus = []
-    for (let i = 0; i < 1; i++) {
-        const haiku = await getHaiku(req);
-        if(!haiku){
-            res.send("Could not get a valid response from the AI.");
+        res.json({message: "Bad Request - need signedMessage in body."});
+        return;
+    }
+    try {
+        if(!await verify(req.body.message, req.body.signature, req.body.accountId)){
+            res.status(400);
+            res.json({message: "Bad Request - signed message verification failed."});
             return;
         }
-        haikus.push(haiku);
+
+        if(!await verifyOwnership(req.body.accountId, req.body.id)){
+            res.status(400);
+            res.json({message: `Bad Request - you don't own the haiku #${req.body.id}.`});
+            return;
+        }
+
+        if(!haikus.has(req.body.id)){
+            newHaikus = []
+            for (let i = 0; i < 1; i++) {
+                const haiku = await getHaiku(req.body.message);
+                if(!haiku){
+                    res.send("Could not get a valid response from the AI.");
+                    return;
+                }
+                newHaikus.push(haiku);
+            }
+            haikus.set(req.body.id, newHaikus);
+        }
+
+        const result = haikus.get(req.body.id);
+
+        if(!result){
+            res.status(400);
+            res.json({message: `Bad Request - Something went wrong. Please try again`});
+            haikus.delete(req.body.id);
+            return;
+        }
+
+        res.send(result); //respond with the array of courses
+    } catch (error) {
+        next(error);
     }
-    res.send([haikus]); //respond with the array of courses
 });
 
 //check if theres the environment variable PORT<br>
